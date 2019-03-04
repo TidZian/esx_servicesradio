c94909857597c00c0221b700b8d895a75b620573
TidZian <48211267+TidZian@users.noreply.github.com>
2019-03-04 14:26:10 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config          = {}
+Config.Locale   = 'fr'
+
+Config.Networks = {
+    { jobname = 'police', joblabel = '~b~LSPD~s~', NetworkId = 2 },
+    { jobname = 'ambulance', joblabel = '~b~LSCoFD~s~', NetworkId = 3 },
+    { jobname = 'busdriver', joblabel = '~y~LST~s~', NetworkId = 4 }
+}
