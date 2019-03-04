c913464bebeb03df984202f97f3056568a872393
TidZian <48211267+TidZian@users.noreply.github.com>
2019-03-04 23:07:27 +0100

    Add files via upload

---
+++ locales/dk.lua
@@ -0,0 +1,7 @@
+Locales ['dk'] = {
+	['title']       	= '~r~Radio %s',
+    ['title_2']     	= ': ~s~%s',
+	['no_network']      = 'Slukket ~r~[Offline]~w~',
+	['network']     	= 'Tændt ~g~[Online]~w~',
+	['who_talk']     	= '~o~Snakker: ~s~%s'
+}
