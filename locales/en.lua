33e5b662e2bb8fba32bac715d8af449bb4879555
TidZian <48211267+TidZian@users.noreply.github.com>
2019-03-04 22:45:09 +0100

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,7 @@
+Locales ['en'] = {
+	['title']       	= '~r~Radio %s',
+    ['title_2']     	= ': ~s~%s',
+	['no_network']      = 'Off ~r~[Offline]~w~',
+	['network']     	= 'On ~g~[Online]~w~',
+	['who_talk']     	= '~o~Voice: ~s~%s'
+}
