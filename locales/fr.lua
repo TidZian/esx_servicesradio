902cbfaece7053bffdcd1861820ac4e3b34b869a
TidZian <48211267+TidZian@users.noreply.github.com>
2019-03-04 14:27:21 +0100

    Add files via upload

---
+++ locales/fr.lua
@@ -0,0 +1,7 @@
+Locales ['fr'] = {
+	['title']       	= '~r~Radio %s',
+    ['title_2']     	= ': ~s~%s',
+	['no_network']      = 'Off ~r~[Hors ligne]~w~',
+	['network']     	= 'Parler ~g~[En ligne]~w~',
+	['who_talk']     	= '~o~Actuellement: ~s~%s'
+}
