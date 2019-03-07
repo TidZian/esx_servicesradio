b218da6016bcfffea6a3b7c29d0e1dae74119e39
TidZian <48211267+TidZian@users.noreply.github.com>
2019-03-07 17:19:35 +0100

    Delete en.lua

--- locales/en.lua
+++
@@ -1,7 +0,0 @@
-Locales ['en'] = {
-	['title']       	= '~r~Radio %s',
-    ['title_2']     	= ': ~s~%s',
-	['no_network']      = 'Off ~r~[Offline]~w~',
-	['network']     	= 'On ~g~[Online]~w~',
-	['who_talk']     	= '~o~Voice: ~s~%s'
-}
